ea7030095cf92ae4c442960acdea6ef7021a666b
Raheema Kadwa <kraheema25@gmail.com>
2014-05-28 14:39:50 -0400

    character

---
+++ v1/Character.java
@@ -0,0 +1,36 @@
+public class Character{
+    private String firstName, lastName, username, gender, password;
+    public ArrayLis
+    
+    public Character(){
+	firstName= "captain";
+	lastName= "underpants";
+	username = "captain28";
+	gender= "male";
+	password= "123";
+    }
+
+
+    public Character (firstName, lastName, username, gender, password){
+    }
+
+    public String getFirst(){
+	return firstName;
+    }
+
+    public String getLast (){
+	return lastName;
+    }
+
+    public String getUsername(){
+	return username;
+    }
+
+    public String getPassword(){
+    }
+
+    
+  
+
+
+}
