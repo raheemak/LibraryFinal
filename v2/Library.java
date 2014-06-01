4b8a496d96584bd813b4f33e8df87383ef756145
Raheema <kraheema25@gmail.com>
2014-06-01 19:53:29 -0400

    adds new book, not sorted yet

--- v2/Library.java
+++ v2/Library.java
@@ -2,51 +2,72 @@ import java.util.*;
 import java.io.*;
 
 public class Library {
-//reading csv file 
+    
 
-    ArrayList books; 
-    //String[][ ] list;
+    private ArrayList<Book>  books= new ArrayList<Book>(); 
+    private int size ;    
     public Library (){
 	books= new ArrayList();
     }
 
-    public static String  makeArray(){
-	//98 books, 2 fields each 
-
-	String [][]list = new String[101][2];
-	int index=0;
-
+    //reading csv file 
+    public  void  makeArray(){
+	//236 books, 2 fields each 
 	try {
 	    File file = new File("Books.txt");
 	    Scanner doc= new Scanner (file);
 	    while (doc.hasNextLine()){
 		String line= doc.nextLine();
-		String[]split = (line.substring(line.indexOf(".")+2)).split(", by ");
-		list[index]= split;
-		index++;
-	    }
+		if (line.equals(null)){
+		}
+		String[]split = line.split(",, ");
+		books.add(new Book(split[0], split[1]));
+		    }
 	}
 	
 	catch (FileNotFoundException e ){
-	    return "boo";
+	    System.out.println("boo");
 	}
+    }
 
-	String z="{";
 
-	for (int x=0; x<100; x++){
-	    z+="{";
-	    z+=list[x][0];
-	    z+=",";
-	    z+=list[x][1];
-	    z+="}";
-	}
-	z+="}";
+    public String toString(){ 
+	String z="";
+	for (int x=0; x<books.size(); x++)
+	    z+=(books.get(x)+"\n");
 	return z;
+    }
+ 
+    /*add book to library 
+    - check if book already exists
+    - place in order*/
+    public void addBook(String title, String author){
+    	books.add(new Book(title, author));
+	String newList="";
+	for (int x=0; x<books.size(); x++)
+	    newList+=(books.get(x)+"\n");
+	try{
+	    FileWriter fstream = new FileWriter("Books.txt");
+	    BufferedWriter out = new BufferedWriter(fstream);
+	    out.write(newList);
+	    out.close();
+	}
+	catch (Exception e){
+	    System.err.println("boo");
+	}
+    }
+ 
+    public int size(){
+	return books.size();
     }
 
     public static void main (String[]args){
-	System.out.println(makeArray());
+	Library test = new Library();
+	test.makeArray();
+	test.addBook("The Fault In Our Stars", "John Green");
+	System.out.println(test);
     }
+    
     
 }
 
