66323a887a16ba6b6d8343134d1ecd9695c15234
Maria Kollaros <marakiimou7@gmail.com>
2014-06-14 14:14:29 -0400

    updating

--- v3/Library.java
+++ v3/Library.java
@@ -112,8 +112,7 @@ public class Library {
 
     public static void main (String[]args){
 	Library test = new Library();
-	test.makeArray();
-	test.addBook("ABC", "George Orwell");
+	test.makeArray();	test.addBook("ABC", "George Orwell");
 	System.out.println(test);
     }
     
